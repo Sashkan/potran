ba249019cd410184c59ad683f6244ce269451ad6
Pierre Olivier Tran <pierreolivier@folk.app>
2023-02-22 10:53:23 +0100

    added reads

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,8 +6,7 @@ export default function AboutPage() {
         Hey, I'm potran, but most people call me PO.
       </p>
       <p>
-        I'm currently lead developer at <b>capsule</b>, building the next
-        generation of sales enablement software.
+        I'm currently working as a freelance developer, mostly with Typescript.
       </p>
       <hr className='h-px my-8 bg-gray-200 border-0 dark:bg-gray-700' />
       <p className='my-5 text-neutral-800 dark:text-neutral-200'>
@@ -18,25 +17,8 @@ export default function AboutPage() {
       <p className='my-5 text-neutral-800 dark:text-neutral-200'>
         I firmly believe in the power of habits, and ownership. After a few
         rough years, I decided to take control of my life and make it better.
+        This blog is a mix of all the teachings I found along the way.
       </p>
-      <p className='my-5 text-neutral-800 dark:text-neutral-200'>
-        Start your day by making your bed, then hit the gym. Take a cold shower,
-        drink a glass of water.
-      </p>
-      <h2 className='my-3 text-neutral-800 dark:text-neutral-200'>
-        Books that changed my life
-      </h2>
-      <ul>
-        <li>
-          <b>Atomic Habits</b>, by James Clear
-        </li>
-        <li>
-          <b>The Power of Habit</b>, by Charles Duhigg
-        </li>
-        <li>
-          <b>The 7 Habits of Highly Effective People</b>, by Stephen Covey
-        </li>
-      </ul>
     </section>
   );
 }
